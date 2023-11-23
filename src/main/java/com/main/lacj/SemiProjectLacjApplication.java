f8b55fb4172b20baa07956d0ffc6daf9c95d8285
sunghyun1014 <sunghyun1014@naver.com>
2023-11-24 00:27:15 +0900

    11 / 24 프로젝트 생성

---
+++ src/main/java/com/main/lacj/SemiProjectLacjApplication.java
@@ -0,0 +1,13 @@
+package com.main.lacj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectLacjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiProjectLacjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/lacj/SemiProjectLacjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.lacj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectLacjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
