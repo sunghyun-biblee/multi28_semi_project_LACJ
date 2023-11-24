8383c3295601b739476b84d3a27bfa48d84e6b30
sunghyun1014 <sunghyun1014@naver.com>
2023-11-24 12:35:26 +0900

    11 /24 컨트롤러 수정

--- src/main/java/com/main/lacj/controller/LacjController.java
+++ src/main/java/com/main/lacj/controller/LacjController.java
@@ -1,8 +1,28 @@
 package com.main.lacj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LacjController {
+//  index.html > login page
 
+	@PostMapping("/regist")
+	public String regist() {
+
+		return "regist";
+	}
+
+	@GetMapping("/mypage")
+	public String mypage() {
+
+		return "mypage";
+	}
+
+	@PostMapping("/mainlist")
+	public String mainlist() {
+
+		return "mainlist";
+	}
 }
