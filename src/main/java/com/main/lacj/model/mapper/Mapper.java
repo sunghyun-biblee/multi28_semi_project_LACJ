412ced66c244193504f2dfdf66e156b45468f1d7
sunghyun1014 <sunghyun1014@naver.com>
2023-11-24 17:21:38 +0900

    회원가입을 하기위해 db에 insert하기 위한 쿼리문을 인터페이스 형식으로 작성

--- src/main/java/com/main/lacj/model/mapper/Mapper.java
+++ src/main/java/com/main/lacj/model/mapper/Mapper.java
@@ -2,13 +2,18 @@ package com.main.lacj.model.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import com.main.lacj.model.dto.BoardDto;
+import com.main.lacj.model.dto.MemberDto;
 
 @org.apache.ibatis.annotations.Mapper
 public interface Mapper {
 
 	@Select(" SELECT * FROM MULTIBOARD ORDER BY MNO DESC ")
 	List<BoardDto> selectList();
+
+	@Insert(" INSERT INTO MULTIMEMBER VALUE(NULL,#{mid},#{mpw},#{mname}) ")
+	public int insertRegi(MemberDto dto);
 }
