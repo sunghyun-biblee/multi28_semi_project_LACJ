e49efdf50aca1d25026576f26031c8b783b81bb2
sunghyun1014 <sunghyun1014@naver.com>
2023-11-24 11:48:07 +0900

    11/24 dto 생성

--- src/main/java/com/main/lacj/dto/MemberDto.java
+++ src/main/java/com/main/lacj/dto/MemberDto.java
@@ -1,8 +1,8 @@
 package com.main.lacj.dto;
 
 public class MemberDto {
-	private int mno;  // primary key
-	private String mid;
-	private String mpw;
-	private String mname;
+    private int mno;  // primary key
+    private String mid;
+    private String mpw;
+    private String mname;
 }
