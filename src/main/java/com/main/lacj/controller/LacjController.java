bec17321b4a3dcae2182cdedd43c4bc435a7ff5e
lokyyyi <jyw9011@naver.com>
2023-11-25 23:22:32 +0900

    11-25

--- src/main/java/com/main/lacj/controller/LacjController.java
+++ src/main/java/com/main/lacj/controller/LacjController.java
@@ -1,17 +1,28 @@
 package com.main.lacj.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.main.lacj.model.biz.Biz;
 import com.main.lacj.model.dto.BoardDto;
 import com.main.lacj.model.dto.MemberDto;
+import com.main.lacj.model.file.FileValidator;
+import com.main.lacj.model.file.UploadFile;
 
 @Controller
 public class LacjController {
@@ -19,6 +30,9 @@ public class LacjController {
 
 	@Autowired
 	private Biz biz;
+	
+	@Autowired
+	private FileValidator fileValidator;
 
 	@RequestMapping("/mainlist")
 	public String mainlist(Model model) {
@@ -30,7 +44,7 @@ public class LacjController {
 	public String registinsert(MemberDto dto) {
 
 		if (biz.insertRegi(dto) > 0) {
-			return "redirect:mainlist";
+			return "login";
 		} else {
 			return "redirect:regist";
 		}
@@ -43,12 +57,22 @@ public class LacjController {
 		return "regist";
 	}
 
-	@GetMapping("/mypage")
-	public String mypage() {
-
-		return "mypage";
+	@RequestMapping("/mypage")
+	public String mypage(HttpSession session) {
+		MemberDto dto = (MemberDto)session.getAttribute("user");
+				
+		if(dto.getMstatus() != "G") {
+			return "mypage";
+		}else {
+			return "mypagefail";	//alert창으로 '게스트는 마이페이지가 존제하지않습니디...'
+		}
 	}
-
+	
+	@RequestMapping("/mypagefail")
+	public String mypagefail() {
+		return "redirect:mainlist";
+	}
+	
 	@RequestMapping("/login")
 	public String login(MemberDto dto, HttpSession session) {
 
@@ -58,11 +82,14 @@ public class LacjController {
 			session.setAttribute("user", login);
 			session.setMaxInactiveInterval(60 * 10);
 			return "redirect:mainlist";
-		} else {
-
+		}else {
 			// 로그인 실패 alert창 처리하기
-			return "login";
+			return "logback";
 		}
+	}
+	@RequestMapping("/loginfail")
+	public String loginfail() {
+		return "login";
 	}
 
 	@RequestMapping("/logout")
@@ -86,18 +113,59 @@ public class LacjController {
 	}
 
 	@PostMapping("/boardinsert")
-	public String boardinsert(BoardDto dto, HttpSession session) {
+	public String boardinsert(BoardDto dto, HttpSession session, UploadFile uploadFile, BindingResult result ) {
 
 		MemberDto logindto = (MemberDto) session.getAttribute("user");
 		int mno = logindto.getMno();
 		if (biz.insertBoard(dto, mno) > 0) {
+			
+			fileValidator.validate(uploadFile, result);
+			if(result.hasErrors()) {
+				return "mainlist";
+			}
+			
+			MultipartFile file = uploadFile.getFile();
+			String filename = file.getOriginalFilename();	//업로드 되어 controller로 넘어온 filedml 실제 이름
+			//System.out.println(filename);
+			
+			UploadFile fileobj = new UploadFile();
+			fileobj.setFilename(filename);
+			fileobj.setDesc(uploadFile.getDesc());
+			
+			InputStream inputStream = null;
+			OutputStream outputStream = null;
+			
+			try {
+				inputStream = file.getInputStream();
+				
+				String path = "C:\\workspace\\6.FrameWork\\SemiProject_LACJ\\src\\main\\resources\\static\\img";
+				File storage = new File(path);
+				if(!storage.exists()) {	//존재여부 확인
+					storage.mkdirs();	//없으면 디렉토리 만들기(폴더생성)
+				}
+				
+				File newfile = new File(path+"/"+filename);
+				if(!newfile.exists()) {
+					newfile.createNewFile();
+				}
+				
+				System.out.println(newfile.getPath());
+				outputStream = new FileOutputStream(newfile);
+				
+				int read = 0;
+				byte[] b = new byte[(int)file.getSize()];
+				while( (read=inputStream.read(b)) != -1 ) {
+					outputStream.write(b,0,read);
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			System.out.println("성공");
 			return "mainlist";
 		} else {
 			System.out.println("실패");
 			return "insert";
 		}
-
 	}
-
 }
