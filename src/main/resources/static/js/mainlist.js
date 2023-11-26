4f1f89a10f76b9a2f027871ae824af75fbf659f1
sunghyun1014 <sunghyun1014@naver.com>
2023-11-27 01:42:10 +0900

    좋아요 기능 99% 구현

--- src/main/resources/static/js/mainlist.js
+++ src/main/resources/static/js/mainlist.js
@@ -1,47 +1,56 @@
-const heartbox = document.querySelectorAll("#likebtn");
-const changeheart = document.getElementById("changbox");
+const heartbox = document.querySelectorAll(".likebtn2");
 
+heartbox.forEach((heartbox) => {
+  heartbox.addEventListener("click", function (event) {
+    heartchange(event, heartbox);
+  });
+});
 
-function heartchange(event) {
+function heartchange(event, clickedButton) {
+  const changeheart = clickedButton.querySelector(".changbox");
   const bno = event.currentTarget.getAttribute("data-id");
-  if (changeheart.classList.contains("likesoff")) {
-    
-    changeheart.classList.remove("likesoff");
-    changeheart.classList.add("likeson");
-	    
+  const likescountElement = clickedButton.querySelector(".likescount"); // 해당 버튼 내의 likescount 엘리먼트 찾기
+
+  if (!likescountElement) {
+    console.error("Error: Likes count element not found!");
+    return; // 요소를 찾지 못한 경우 함수를 여기서 종료
+  }
+
+  if (changeheart.classList.contains("off")) {
+    changeheart.classList.remove("off");
+    changeheart.classList.add("on");
+
     $.ajax({
       type: "POST",
       url: "/uplikes",
       contentType: "application/json",
-      data: JSON.stringify({"bno": bno}),
+      data: JSON.stringify({ bno: bno }),
       success: function (msg) {
-        console.log("성현");
+        console.log("성공");
         console.log(msg.count);
-        $('#likescount').text(msg.count);
+        likescountElement.textContent = msg.count; // 해당 버튼 내의 likescount 엘리먼트의 텍스트 변경
+        $(likescountElement).text(msg.count);
       },
       error: function () {
         alert("오류가 발생했습니다.");
-      }
-    }); 
+      },
+    });
   } else {
-    changeheart.classList.remove("likeson");
-    changeheart.classList.add("likesoff");
+    changeheart.classList.remove("on");
+    changeheart.classList.add("off");
+
     $.ajax({
       type: "POST",
       url: "/downlikes",
       contentType: "application/json",
-      data: JSON.stringify({"bno": bno}),
+      data: JSON.stringify({ bno: bno }),
       success: function (msg) {
         console.log("성공");
-        $('#likescount').text(msg.count);
+        likescountElement.textContent = msg.count; // 해당 버튼 내의 likescount 엘리먼트의 텍스트 변경
       },
       error: function () {
         alert("오류가 발생했습니다.");
-      }
+      },
     });
   }
 }
-
-heartbox.forEach((button) => {
-  button.addEventListener("click", heartchange);
-});
