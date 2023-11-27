4b858b1fc102a2c7a3c3528cf738ec0f83626497
lokyyyi <jyw9011@naver.com>
2023-11-28 00:26:10 +0900

    오류해결

--- src/main/java/com/main/lacj/model/biz/Biz.java
+++ src/main/java/com/main/lacj/model/biz/Biz.java
@@ -13,7 +13,7 @@ public interface Biz {
 	public List<BoardDto> selectList();
 	public MemberDto selectOne(int mno);
 	public int insertRegi(MemberDto dto);
-	public int insertBoard(@Param("btitle") String btitle, @Param("bcontent")String bcontent ,@Param("bimg")String bimg, @Param("mno")int mno);
+	public int insertBoard(@Param("btitle") String btitle, @Param("bcontent")String bcontent ,@Param("bimg")String bimg, @Param("mno")int mno,@Param("bpublic")String bpublic);
 	public int update(MemberDto dto);
 	public int delete(int mno);
     public MemberDto selectLogin(MemberDto dto);
@@ -21,6 +21,7 @@ public interface Biz {
     public int likesUp(int blikes, int bno);
     public BoardDto boardSelectOne(int bno);
     public List<BoardDto> getBoards(int offset, int pageSize);
+    public List<BoardDto> getGuestBoards(int offset, int pageSize);
     public int getBoardCount();
     public void memberDelete(int mno);
     List<BoardDto> getMyWrite(int mno);
