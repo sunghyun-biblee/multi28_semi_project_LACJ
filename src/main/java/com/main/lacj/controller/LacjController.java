75b8d7537b8c663a61127ca1279b5f6c4c06dcb4
sunghyun1014 <sunghyun1014@naver.com>
2023-11-28 12:05:36 +0900

    11/28 최종

--- src/main/java/com/main/lacj/controller/LacjController.java
+++ src/main/java/com/main/lacj/controller/LacjController.java
@@ -71,7 +71,7 @@ public class LacjController {
                 try {
                     inputStream = file.getInputStream();
 
-                    String path = "C:\\workspace\\6.FrameWork\\SemiProject_LACJ\\src\\main\\resources\\static\\img\\profile";
+                    String path = "C:\\workspace\\6_FrameWork\\SemiProject_LACJ\\src\\main\\resources\\static\\img\\profile";
                     File storage = new File(path);
                     if(!storage.exists()) {    //존재여부 확인
                         storage.mkdirs();    //없으면 디렉토리 만들기(폴더생성)
@@ -397,7 +397,7 @@ public class LacjController {
 			try {
 				inputStream = file.getInputStream();
 				
-				String path = "C:\\workspace\\6.FrameWork\\SemiProject_LACJ\\src\\main\\resources\\static\\img";
+				String path = "C:\\workspace\\6_FrameWork\\SemiProject_LACJ\\src\\main\\resources\\static\\img";
 				File storage = new File(path);
 				if(!storage.exists()) {	//존재여부 확인
 					storage.mkdirs();	//없으면 디렉토리 만들기(폴더생성)
